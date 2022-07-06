6e1a2f15af4d30ab0c3d5a1ba56a3cab57fe200f
jermaine buford <bufordjermaine@outlook.com>
2022-07-06 10:09:55 -0400

    first commit

---
+++ openweatherapp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace openweatherapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var api = ($"https://api.openweathermap.org/data/2.5/weather?zip={40213},&appid=4abdc9782e101166589788e71986b686");
+            
+        }
+    }
+}
